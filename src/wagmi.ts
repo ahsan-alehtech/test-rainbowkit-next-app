43c8950efaff983c48e145c5f00d5bf17426860b
ahsan-alehtech <ahsaniqbal@aleh.tech>
2025-07-16 17:13:55 +0500

    Version updates

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,25 +1,36 @@
-import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import {
-  arbitrum,
-  base,
-  mainnet,
-  optimism,
-  polygon,
-  sepolia,
-} from 'wagmi/chains';
+import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { http } from "viem";
+import { arbitrum, base, mainnet, optimism, polygon, sepolia } from "wagmi/chains";
+import { injectedWallet } from "@rainbow-me/rainbowkit/wallets";
 
 const projectId = "a2f589ed77cb7532a3e3a964ab5b0ba5";
 
+// export const config = getDefaultConfig({
+//   appName: 'RainbowKit App',
+//   projectId,
+//   chains: [
+//     mainnet,
+//     polygon,
+//     optimism,
+//     arbitrum,
+//     base,
+//     ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
+//   ],
+//   ssr: true,
+// });
+
 export const config = getDefaultConfig({
-  appName: 'RainbowKit App',
+  appName: "Space",
   projectId,
-  chains: [
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    base,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
+  chains: [base],
+  transports: {
+    [base.id]: http(),
+  },
+  wallets: [
+    {
+      groupName: "Injected",
+      wallets: [injectedWallet],
+    },
   ],
   ssr: true,
 });
