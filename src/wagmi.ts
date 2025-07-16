f893650cbf05bf3e77d1f3bb60f974d37529e40b
ahsan-alehtech <ahsaniqbal@aleh.tech>
2025-07-16 17:04:42 +0500

    initial setup

---
+++ src/wagmi.ts
@@ -0,0 +1,25 @@
+import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+import {
+  arbitrum,
+  base,
+  mainnet,
+  optimism,
+  polygon,
+  sepolia,
+} from 'wagmi/chains';
+
+const projectId = "a2f589ed77cb7532a3e3a964ab5b0ba5";
+
+export const config = getDefaultConfig({
+  appName: 'RainbowKit App',
+  projectId,
+  chains: [
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    base,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
+  ],
+  ssr: true,
+});
